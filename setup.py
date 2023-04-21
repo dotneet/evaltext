3a1b3ca736c8e61294609b792fdccc3ba449249c
Shinji Yamada <dotneet@gmail.com>
2023-04-21 19:32:33 +0900

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+from os import path
+
 from setuptools import setup, find_packages
+
+root_dir = path.abspath(path.dirname(__file__))
+
+
+def _requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
+
+
+def _test_requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'test-requirements.txt')).readlines()]
+
 
 setup(
     name='evaltext',
     version='0.1.0',
+    license='MIT',
     description='prompt evaluation tool.',
     author='Shinji Yamada',
     author_email='dotneet@gmail.com',
@@ -15,12 +29,6 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.8',
-    install_requires=[
-        'numpy',
-        'pandas',
-        'openai',
-        'cohere',
-        'python-dotenv',
-        'tiktoken'
-    ]
+    install_requires=_requirements(),
+    tests_require=_test_requirements(),
 )
