ecbd00f0b9fba2a7f5eadd1129c22d59ecf486d4
Shinji Yamada <dotneet@gmail.com>
2023-04-27 22:26:12 +0900

    feat: plot scores

--- evaltext/jsonl.py
+++ evaltext/jsonl.py
@@ -1,5 +1,10 @@
+import datetime
 import json
 from typing import Callable
+
+import jsonlines
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def read_jsonl_records(file_path: str):
@@ -15,3 +20,20 @@ def read_jsonl_record(file_path: str, line_number: int):
 def eval_jsonl_item(file_path: str, line_number: int, scoring_fn: Callable[[str], float]):
     record = read_jsonl_record(file_path, line_number)['response']
     return scoring_fn(record['response'])
+
+def plot_scores_by_model(file_path: str):
+    data = []
+    with jsonlines.open(file_path) as reader:
+        for item in reader:
+            data.append(item)
+
+    df = pd.DataFrame(data)
+
+    df['time'] = pd.to_datetime(df['time'])
+    print(df)
+    df['time'] = df['time'].apply(lambda x: x.strftime('%Y-%m-%d %H'))
+    _, ax = plt.subplots()
+    for name, group in df.groupby('model_name'):
+        group.plot(x='time', y='score', ax=ax, label=name)
+
+    plt.show()
